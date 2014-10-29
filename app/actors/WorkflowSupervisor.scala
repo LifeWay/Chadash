904e07c8eeb7788ecc3210d5e616543860a20cd5
Ryan Means <ryan@rmeans.com>
2014-10-29 15:48:50 -0500

    more framing...

--- app/actors/WorkflowSupervisor.scala
+++ app/actors/WorkflowSupervisor.scala
@@ -1,10 +1,12 @@
 package actors
 
 import actors.DeploymentSupervisor.{Deploy, Started, WorkflowInProgress}
-import actors.workflow.AutoScalingGroup.{CreateASG, ASGCreated}
+import actors.workflow.AutoScalingGroup.{ASGCreated, CreateASG}
 import actors.workflow.ElasticLoadBalancer.{CreateELB, ELBCreated}
 import actors.workflow.LaunchConfiguration.{CreateLaunchConfig, LaunchConfigCreated}
-import actors.workflow.{AutoScalingGroup, ElasticLoadBalancer, LaunchConfiguration}
+import actors.workflow.Route53Switch.{RequestRoute53Switch, Route53SwitchCompleted}
+import actors.workflow.WarmUp.{WaitForWarmUp, WarmUpCompleted}
+import actors.workflow._
 import akka.actor.SupervisorStrategy._
 import akka.actor._
 
@@ -46,6 +48,7 @@ class WorkflowSupervisor extends Actor with ActorLogging {
     case x: LaunchConfigCreated => {
       log.debug("Launch configuration created...")
       stopChild(sender())
+
       val elb = context.actorOf(Props[ElasticLoadBalancer], "createELB")
       context.watch(elb)
       elb ! CreateELB(deployment.appName)
@@ -62,12 +65,29 @@ class WorkflowSupervisor extends Actor with ActorLogging {
       log.debug("ASG created...")
       stopChild(sender())
 
+      val warmup = context.actorOf(Props[WarmUp], "warmup")
+      context.watch(warmup)
+      warmup ! WaitForWarmUp(deployment.appName)
+    }
+    case x: WarmUpCompleted => {
+      log.debug("ELB warmup completed...")
+      stopChild(sender())
+
+      val route53switch = context.actorOf(Props[Route53Switch], "route53switch")
+      context.watch(route53switch)
+      route53switch ! RequestRoute53Switch(deployment.appName)
+    }
+    case x: Route53SwitchCompleted => {
+      log.debug("Route 53 switch has completed. We are finished!")
+      stopChild(sender())
+
       become(receive)
     }
     case Terminated(actorRef) => {
-      log.debug("One of our children has died...the deployment has failed" + actorRef.toString())
+      log.debug(s"One of our children has died...the deployment has failed and needs a human ${actorRef.toString}")
       context.parent ! DeploymentSupervisor.DeployFailed
     }
+    case m: Any => log.debug(s"unknown message type received. ${m.toString}")
   }
 
   def stopChild(ref: ActorRef) = {
---
+++ app/actors/workflow/WarmUp.scala
@@ -0,0 +1,67 @@
+package actors.workflow
+
+import actors.workflow.WarmUp.{CheckWarmUp, WaitForWarmUp, WarmUpCompleted}
+import akka.actor.{Actor, ActorLogging}
+
+import scala.concurrent.duration._
+
+class WarmUp extends Actor with ActorLogging {
+
+  import context._
+
+  var temporaryTestCounter = 0
+
+  var appVersion: String = ""
+
+  override def receive: Receive = {
+    case x: WaitForWarmUp => {
+      log.debug("waiting for warm-up...")
+      appVersion = x.appVersion
+      become(warming)
+
+      self ! CheckWarmUp
+    }
+  }
+
+  def warming: Receive = {
+    case x: WaitForWarmUp => {
+      log.debug("already waiting for the warm up to pass.... ignoring message")
+    }
+    case CheckWarmUp => {
+      log.debug("checking ELB health...")
+      //TODO: start ticking, checking every 15 seconds to see if instances are all healthy in the ELB.
+      if (temporaryTestCounter < 2) {
+        temporaryTestCounter = temporaryTestCounter + 1
+        system.scheduler.scheduleOnce(15 seconds, self, CheckWarmUp)
+      } else {
+        log.debug("we have completed... notify upstream")
+        parent ! WarmUpCompleted(appVersion)
+        become(receive)
+      }
+    }
+  }
+
+  override def postRestart(reason: Throwable): Unit = {
+    super.postRestart(reason)
+    log.info(s"Restarted because of ${reason.getMessage}")
+  }
+
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+
+    message.get match {
+      case x: WaitForWarmUp => context.system.scheduler.scheduleOnce(10 seconds, self, x)
+      case _ => log.warning("Actor restarting, but message is not being replayed.")
+    }
+  }
+}
+
+object WarmUp {
+
+  case class WaitForWarmUp(appVersion: String)
+
+  case class WarmUpCompleted(appVersion: String)
+
+  case object CheckWarmUp
+
+}
+
