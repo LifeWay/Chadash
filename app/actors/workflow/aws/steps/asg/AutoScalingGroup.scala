f70ee663b60c355755d98d9a3763fcc772093e35
Ryan Means <ryan@rmeans.com>
2014-11-12 16:28:04 -0600

    starting in on ASG creation.

--- app/actors/workflow/aws/steps/asg/AutoScalingGroup.scala
+++ app/actors/workflow/aws/steps/asg/AutoScalingGroup.scala
@@ -1,18 +1,57 @@
 package actors.workflow.aws.steps.asg
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, Props}
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.services.autoscaling.AmazonAutoScalingClient
+import com.amazonaws.services.autoscaling.model.{CreateAutoScalingGroupRequest, Tag}
 
+import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 
-class AutoScalingGroup extends Actor with ActorLogging {
+class AutoScalingGroup(credentials: AWSCredentials) extends Actor with ActorLogging {
 
-  import AutoScalingGroup._
+  import actors.workflow.aws.steps.asg.AutoScalingGroup._
 
   override def receive: Receive = {
-    case x: CreateASG => {
-      log.debug("Attempting to create autoscaling group....")
-      context.parent ! ASGCreated(x.appVersion)
-    }
+    case x: CreateASG =>
+      val asgRequest = new CreateAutoScalingGroupRequest()
+        .withAutoScalingGroupName(x.labelName)
+        .withDesiredCapacity(x.desiredCapacity)
+        .withHealthCheckGracePeriod(x.healthCheckGracePeriod)
+        .withHealthCheckType(x.healthCheckType)
+        .withLaunchConfigurationName(x.labelName)
+        .withLoadBalancerNames(x.labelName)
+        .withMaxSize(x.maxSize)
+        .withMinSize(x.minSize)
+
+      x.defaultCoolDown match {
+        case Some(y) => asgRequest.withDefaultCooldown(y)
+        case None => ()
+      }
+
+      x.placementGroup match {
+        case Some(y) => asgRequest.withPlacementGroup(y)
+        case None => ()
+      }
+
+      x.tags match {
+        case Some(y) => {
+          asgRequest.setTags(y.seq.foldLeft(Seq.empty[Tag])((x, i) =>
+            x :+ new Tag().withKey(i._1).withValue(i._2)
+          ).asJava)
+        }
+        case None => ()
+      }
+
+      x.terminationPolicies match {
+        case Some(y) => asgRequest.withTerminationPolicies(y.asJava)
+        case None => ()
+      }
+
+      val awsClient = new AmazonAutoScalingClient(credentials)
+      awsClient.createAutoScalingGroup(asgRequest)
+
+      context.parent ! ASGCreated
   }
 
   override def postRestart(reason: Throwable): Unit = {
@@ -32,8 +71,20 @@ class AutoScalingGroup extends Actor with ActorLogging {
 
 object AutoScalingGroup {
 
-  case class CreateASG(appVersion: String)
+  case class CreateASG(labelName: String,
+                       desiredCapacity: Int,
+                       minSize: Int,
+                       maxSize: Int,
+                       healthCheckGracePeriod: Int,
+                       healthCheckType: String,
+                       vpcSubnets: Seq[String],
+                       tags: Option[Seq[(String, String)]] = None,
+                       defaultCoolDown: Option[Int] = None,
+                       placementGroup: Option[String] = None,
+                       terminationPolicies: Option[Seq[String]] = None
+                        )
 
-  case class ASGCreated(appVersion: String)
+  case object ASGCreated
 
+  def props(creds: AWSCredentials): Props = Props(new AutoScalingGroup(creds))
 }
