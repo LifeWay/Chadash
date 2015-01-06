5c85db273a492de915897540e580d78d12a51cad
Ryan Means <ryan@rmeans.com>
2015-01-06 08:25:54 -0600

    some clean-up, preparing for next step in launching a new stack.

--- app/actors/workflow/steps/validateandfreeze/ValidateAndFreezeSupervisor.scala
+++ app/actors/workflow/steps/validateandfreeze/ValidateAndFreezeSupervisor.scala
@@ -6,11 +6,14 @@ import actors.workflow.aws.{AWSSupervisorStrategy, AWSWorkflow}
 import actors.workflow.steps.validateandfreeze.FreezeASG.{ASGFrozen, FreezeASGWithName}
 import actors.workflow.steps.validateandfreeze.GetASGName.{ASGForStack, GetASGNameForStackName}
 import actors.workflow.steps.validateandfreeze.StackList.{FilteredStacks, ListNonDeletedStacksStartingWithName}
-import actors.workflow.steps.validateandfreeze.ValidateAndFreezeSupervisor.MoreThanOneActiveStack
+import actors.workflow.steps.validateandfreeze.ValidateAndFreezeSupervisor.{NoPreviousStack, StackFrozen, MoreThanOneActiveStack}
 import akka.actor.{Actor, ActorLogging, Props, Terminated}
 import com.amazonaws.auth.AWSCredentials
 
 class ValidateAndFreezeSupervisor(credentials: AWSCredentials) extends Actor with ActorLogging with AWSSupervisorStrategy {
+
+  var stackName: Option[String] = None
+
   override def receive: Receive = {
     case step: StartStep =>
       val stackList = context.actorOf(StackList.props(credentials), "stackList")
@@ -26,13 +29,19 @@ class ValidateAndFreezeSupervisor(credentials: AWSCredentials) extends Actor wit
       context.stop(sender())
 
       x.stackList.length match {
-        case i if i > 1 => context.parent ! MoreThanOneActiveStack
+        case i if i > 1 =>
+          context.parent ! LogMessage(s"Error: More than one active version of this stack is running")
+          context.parent ! MoreThanOneActiveStack
         case 1 =>
+          context.parent ! LogMessage(s"One running stack found, querying for the ASG name")
           val asgFetcher = context.actorOf(GetASGName.props(credentials), "getASGName")
           context.watch(asgFetcher)
-          asgFetcher ! GetASGNameForStackName(x.stackList(0))
+          val stack = x.stackList(0)
+          stackName = Some(stack)
+          asgFetcher ! GetASGNameForStackName(stack)
         case 0 =>
-          context.parent ! LogMessage("No current stack for given name found")
+          context.parent ! LogMessage(s"No previous stack found, this is the first deployment of this stack.")
+          context.parent ! NoPreviousStack
       }
 
     case x: ASGForStack =>
@@ -43,10 +52,17 @@ class ValidateAndFreezeSupervisor(credentials: AWSCredentials) extends Actor wit
       context.watch(asgFreezer)
 
       asgFreezer ! FreezeASGWithName(x.asgName)
-      context.parent ! LogMessage(s"Requesting to freeze ${x.asgName}")
+      context.parent ! LogMessage(s"ASG found, Requesting to suspend scaling activities ${x.asgName}")
 
     case x: ASGFrozen =>
+      context.unwatch(sender())
+      context.stop(sender())
+
       context.parent ! LogMessage(s"ASG ${x.asgName} has been frozen for deployment")
+      stackName match {
+        case Some(stack) => context.parent ! StackFrozen(stack, x.asgName)
+        case None => throw new Exception("stack name a None when this should not have been possible")
+      }
 
     case Terminated(actorRef) =>
       context.parent ! LogMessage(s"Child actor has died unexpectedly. Need a human! Details: ${actorRef.toString()}")
@@ -55,6 +71,10 @@ class ValidateAndFreezeSupervisor(credentials: AWSCredentials) extends Actor wit
 }
 
 object ValidateAndFreezeSupervisor {
+
+  case class StackFrozen(stackName: String, asgName: String)
+
+  case object NoPreviousStack
 
   case object MoreThanOneActiveStack
 
