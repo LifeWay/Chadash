67a3ba7ed14760e69a46da1c090614b83d9b5df8
Ryan Means <ryan@rmeans.com>
2015-02-17 15:22:43 -0600

    incorrect understanding of supervisor strategy params. This strategy allows purely a set number of restarts - 20, in Chadash's case, restarts done via the AWSRestartableActor trait are spaced 15 seconds apart, so this comes to ~ 5 min of allowed restart attempts before the deploy will fail.

--- app/actors/workflow/AWSSupervisorStrategy.scala
+++ app/actors/workflow/AWSSupervisorStrategy.scala
@@ -8,13 +8,14 @@ import com.amazonaws.{AmazonClientException, AmazonServiceException}
 import scala.concurrent.duration._
 
 trait AWSSupervisorStrategy extends Actor with ActorLogging {
-  override val supervisorStrategy = OneForOneStrategy(withinTimeRange = 5.minutes, loggingEnabled = false) {
+  override val supervisorStrategy = OneForOneStrategy(maxNrOfRetries = 20, loggingEnabled = true) {
     case ex: AmazonServiceException =>
       context.parent ! LogMessage(ex.toString)
       log.error(ex, "Unrecoverable Amazon Exception")
       Stop
 
     case _: AmazonClientException =>
+      log.debug("Supervisor Authorized Restart")
       Restart
 
     case ex: Exception =>
