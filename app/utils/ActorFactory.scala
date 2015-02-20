07b09783868a6cfbbb76d2bb4cdf59ee63502f80
Ryan Means <ryan@rmeans.com>
2015-02-20 07:55:34 -0600

    this pattern just became a whole lot cleaner.. nice and concise for "production" cases. For test cases, I'll just match on the specific CompanionObject types I want to control for ActorProbes and mocks while passing through the rest.

--- app/utils/ActorFactory.scala
+++ app/utils/ActorFactory.scala
@@ -1,21 +1,17 @@
 package utils
 
-import actors.workflow.tasks.ASGInfo
 import akka.actor._
-
-trait ActorFactory {
-  def apply[T <: PropFactory](ref: T, context: ActorRefFactory, name: String, args: Any*): ActorRef
-}
 
 trait PropFactory {
   def props(args: Any*): Props
 }
 
+trait ActorFactory {
+  def apply[T <: PropFactory](ref: T, context: ActorRefFactory, name: String, args: Any*): ActorRef
+}
+
 object ActorFactory extends ActorFactory {
   def apply[T <: PropFactory](ref: T, context: ActorRefFactory, name: String, args: Any*): ActorRef = {
-    ref match {
-      case ASGInfo => context.actorOf(ASGInfo.props(args), name)
-      case x: Any => throw new UnsupportedOperationException("Unmatched type: " + x.getClass.toGenericString)
-    }
+    context.actorOf(ref.props(args), name)
   }
 }
