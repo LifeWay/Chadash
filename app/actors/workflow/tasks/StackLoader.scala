d87d413059e3a354e4615b2295562a5f01e62469
Ryan Means <ryan.means@lifeway.com>
2018-03-20 21:30:48 -0500

    stack loader to now also return an option of tags for a given stack.

--- app/actors/workflow/tasks/StackLoader.scala
+++ app/actors/workflow/tasks/StackLoader.scala
@@ -3,12 +3,25 @@ package actors.workflow.tasks
 import actors.workflow.AWSRestartableActor
 import akka.actor.Props
 import com.amazonaws.auth.AWSCredentialsProvider
-import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.S3Object
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.{AmazonS3Exception, S3Object}
 import org.apache.commons.io.IOUtils
-import play.api.Logger
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 import utils.{AmazonS3Service, PropFactory}
+
+case class Tag(key: String, value: String)
+
+object Tag {
+  implicit val reads: Reads[Tag] = (
+    (JsPath \ "Key").read[String] and
+      (JsPath \ "Value").read[String]
+  )(Tag.apply _)
+  implicit val writes: Writes[Tag] = (
+    (JsPath \ "Key").write[String] and
+      (JsPath \ "Value").write[String]
+  )(unlift(Tag.unapply))
+}
 
 class StackLoader(credentials: AWSCredentialsProvider, bucketName: String) extends AWSRestartableActor with AmazonS3Service {
 
@@ -18,15 +31,26 @@ class StackLoader(credentials: AWSCredentialsProvider, bucketName: String) exten
     case msg: LoadStack =>
       val client = s3Client(credentials)
       val stackObject: S3Object = client.getObject(bucketName, s"chadash-stacks/${msg.stackPath}.json")
+      val tags = getTags(bucketName, s"chadash-stacks/${msg.stackPath}.tags.json", client)
       val stackFileJson = Json.parse(IOUtils.toByteArray(stackObject.getObjectContent))
 
-      context.parent ! StackLoaded(stackFileJson)
+      context.parent ! StackLoaded(stackFileJson, tags)
+  }
+
+  def getTags(bucketName: String, path: String, client: AmazonS3): Option[Seq[Tag]] = {
+    try {
+      val tagObject: S3Object = client.getObject(bucketName, path)
+      Json.parse(IOUtils.toByteArray(tagObject.getObjectContent)).asOpt[Seq[Tag]]
+    } catch {
+      case _:AmazonS3Exception => None
+      case e:Throwable => throw e
+    }
   }
 }
 
 object StackLoader extends PropFactory {
   case class LoadStack(stackPath: String)
-  case class StackLoaded(stackJson: JsValue)
+  case class StackLoaded(stackJson: JsValue, tags: Option[Seq[Tag]])
 
   override def props(args: Any*): Props = Props(classOf[StackLoader], args: _*)
 }
--- test/actors/workflow/tasks/StackLoaderSpec.scala
+++ test/actors/workflow/tasks/StackLoaderSpec.scala
@@ -8,7 +8,7 @@ import akka.actor.{ActorRef, ActorRefFactory, ActorSystem, Props}
 import akka.testkit.{TestKit, TestProbe}
 import com.amazonaws.auth.AWSCredentialsProvider
 import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.model.S3Object
+import com.amazonaws.services.s3.model.{AmazonS3Exception, S3Object}
 import com.amazonaws.{AmazonClientException, AmazonServiceException}
 import org.mockito.Mockito
 import org.scalatest.mock.MockitoSugar
@@ -27,25 +27,52 @@ class StackLoaderSpec extends TestKit(ActorSystem("TestKit", TestConfiguration.t
   s3successObject.setKey("chadash-stacks/test-success.json")
   s3successObject.setObjectContent(new ByteArrayInputStream(Json.obj("test" -> JsString("success")).toString().getBytes("UTF-8")))
 
+  val s3successObjectWithTags = new S3Object()
+  s3successObjectWithTags.setBucketName("test-bucket-name")
+  s3successObjectWithTags.setKey("chadash-stacks/test-success-with-tags.json")
+  s3successObjectWithTags.setObjectContent(new ByteArrayInputStream(Json.obj("test" -> JsString("success")).toString().getBytes("UTF-8")))
+
+  val s3successTags = new S3Object()
+  s3successTags.setBucketName("test-bucket-name")
+  s3successTags.setKey("chadash-stacks/test-success-with-tags.tags.json")
+  s3successTags.setObjectContent(new ByteArrayInputStream(
+    Json.arr(
+      Json.obj("Key" -> JsString("Project"), "Value" -> JsString("Chadash")),
+      Json.obj("Key" -> JsString("Owner"), "Value" -> JsString("LifeWay"))
+    ).toString().getBytes("UTF-8")
+  ))
+
   val s3restartObject = new S3Object()
   s3restartObject.setBucketName("test-bucket-name")
   s3restartObject.setKey("chadash-stacks/test-aws-restart.json")
   s3restartObject.setObjectContent(new ByteArrayInputStream(Json.obj("test" -> JsString("success")).toString().getBytes("UTF-8")))
 
   Mockito.doReturn(s3successObject).when(mockedClient).getObject("test-bucket-name", "chadash-stacks/test-success.json")
+  Mockito.doThrow(new AmazonS3Exception("not found")).when(mockedClient).getObject("test-bucket-name", "chadash-stacks/test-success.tags.json")
+  Mockito.doReturn(s3successObjectWithTags).when(mockedClient).getObject("test-bucket-name", "chadash-stacks/test-success-with-tags.json")
+  Mockito.doReturn(s3successTags).when(mockedClient).getObject("test-bucket-name", "chadash-stacks/test-success-with-tags.tags.json")
   Mockito.doThrow(new AmazonServiceException("failed")).when(mockedClient).getObject("test-bucket-name", "chadash-stacks/test-aws-down.json")
   Mockito.doThrow(new AmazonClientException("connection problems")).doReturn(s3restartObject).when(mockedClient).getObject("test-bucket-name", "chadash-stacks/test-aws-restart.json")
+  Mockito.doThrow(new AmazonS3Exception("not found")).when(mockedClient).getObject("test-bucket-name", "chadash-stacks/test-aws-restart.tags.json")
 
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
 
-  "A StackLoader actor" should "return a JSON value for a valid stack" in {
+  "A StackLoader actor" should "return a JSON value for a valid stack and no tags" in {
     val probe = TestProbe()
     val proxy = TaskProxyBuilder(probe, StackLoader, system, TestActorFactory)
 
     probe.send(proxy, LoadStack("test-success"))
-    probe.expectMsg(StackLoaded(Json.obj("test" -> JsString("success"))))
+    probe.expectMsg(StackLoaded(Json.obj("test" -> JsString("success")), None))
+  }
+
+  "A StackLoader actor" should "return a JSON value for a valid stack with tags when provided" in {
+    val probe = TestProbe()
+    val proxy = TaskProxyBuilder(probe, StackLoader, system, TestActorFactory)
+
+    probe.send(proxy, LoadStack("test-success-with-tags"))
+    probe.expectMsg(StackLoaded(Json.obj("test" -> JsString("success")), Some(Seq(Tag("Project", "Chadash"), Tag("Owner", "LifeWay")))))
   }
 
   it should "throw an exception if AWS is down" in {
@@ -62,7 +89,7 @@ class StackLoaderSpec extends TestKit(ActorSystem("TestKit", TestConfiguration.t
     val proxy = TaskProxyBuilder(probe, StackLoader, system, TestActorFactory)
 
     probe.send(proxy, LoadStack("test-aws-restart"))
-    probe.expectMsg(StackLoaded(Json.obj("test" -> JsString("success"))))
+    probe.expectMsg(StackLoaded(Json.obj("test" -> JsString("success")), None))
   }
 
   val props = Props(new StackLoader(null, "test-bucket-name") {
